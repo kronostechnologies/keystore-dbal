c5a3380e43a361ab6ff92b4bd6b489c6e7bf1329
jbreton <jbreton@kronostechnologies.com>
2017-05-09 11:32:18 -0400

    Adds factory

---
+++ src/DBAL/Factory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Kronos\Keystore\Repository\DBAL;
+
+use Doctrine\DBAL\Connection;
+
+class Factory {
+
+	/**
+	 * @param Connection $connection
+	 * @param $tableName
+	 * @param $keyField
+	 * @param $valueField
+	 * @return Adaptor
+	 */
+	public function createDBALAdaptor(Connection $connection, $tableName, $keyField, $valueField) {
+		return new Adaptor($connection, $tableName, $keyField, $valueField);
+	}
+}
